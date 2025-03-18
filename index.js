b238f0c7248f4991bd1976105216888cd22bcb03
Michael-Ngochi <ngoochimike@gmail.com>
2025-03-18 21:52:04 +0300

    added global and local scopes

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName="bob"
+
+function upperCaseCustomerName(){
+    customerName=customerName.toUpperCase();
+    return customerName;
+}
+//console.log(upperCaseCustomerName());
+
+function setBestCustomer(){
+    bestCustomer='not bob';
+}
+
+function overwriteBestCustomer(){
+bestCustomer="maybe bob";
+}
+
+const leastFavoriteCustomer="Pip"
+
+function changeLeastFavoriteCustomer(){
+leastFavoriteCustomer="John"
+}
